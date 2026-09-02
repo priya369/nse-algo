0a371507b029946351f522ed2bc7dd04fc9f9d60
priyabrata jena <39461119+priya369@users.noreply.github.com>
2026-09-02 20:45:34 +0530

    changing dataset id name

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,7 +17,7 @@ provider "google" {
 
 resource "google_bigquery_dataset" "stock_market" {
   project    = var.project_id
-  dataset_id = "stock_market"
+  dataset_id = "nse-raw-data"
   location   = var.bigquery_location
 }
 
